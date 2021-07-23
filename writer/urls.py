339ce21f4f32ed3150801798de25e0fbd6b0e18d
RI Rijan <38884716+DeveloperRijan@users.noreply.github.com>
2021-07-23 19:08:03 +0600

    unnecessary files removed from writer panel | organized writer panel and templates layouts

--- writer/urls.py
+++ writer/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("writer/dashboard/", views.dashboard, name='writer.dashboard')
+    path("writer/dashboard/", views.dashboard, name='writer.dashboard'),
+    path("writer/new-post/", views.post_new, name='writer.post.new'),
+    path("writer/posts/", views.posts, name='writer.posts'),
 ]
--- writer/views.py
+++ writer/views.py
@@ -5,3 +5,8 @@ from django.http import HttpResponse
 # Create your views here.
 def dashboard(request):
     return render(request, "writer/dashboard.html")
+
+
+# add new post
+def post_new(request):
+    return render(request, "writer/pages/posts/new.html")
